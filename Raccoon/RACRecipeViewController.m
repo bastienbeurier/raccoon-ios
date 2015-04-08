7f297e5dcb0c63d8dc0fcaebc3a4de3e703cb1b0
Bastien Beurier <bastienbeurier@gmail.com>
2015-04-08 09:40:29 -0700

    Recipe VC design.

--- Raccoon/RACRecipeViewController.m
+++ Raccoon/RACRecipeViewController.m
@@ -26,9 +26,16 @@
 
 @property (weak, nonatomic) IBOutlet UIView *navBar;
 @property (weak, nonatomic) IBOutlet UIScrollView *scrollView;
-@property (strong, nonatomic) UIImageView *imageView;
+@property (weak, nonatomic) IBOutlet UIImageView *imageView;
 @property (weak, nonatomic) IBOutlet UILabel *titleView;
 @property (strong, nonatomic) UIView *imageTitleContainer;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *imageWidthConstraint;
+@property (weak, nonatomic) IBOutlet UILabel *firstInfoTitle;
+@property (weak, nonatomic) IBOutlet UILabel *secondInfoTitle;
+@property (weak, nonatomic) IBOutlet UILabel *thirdInfoTitle;
+@property (weak, nonatomic) IBOutlet UILabel *firstInfoView;
+@property (weak, nonatomic) IBOutlet UILabel *secondInfoView;
+@property (weak, nonatomic) IBOutlet UILabel *thirdInfoView;
 
 @end
 
@@ -37,91 +44,39 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
-    self.scrollView.contentSize = CGSizeMake(self.view.frame.size.width, 1000);
-    
     self.scrollView.delegate = self;
     
-    //Create image view.
-    self.imageView = [[UIImageView alloc] initWithFrame:CGRectMake(0, 0, self.scrollView.contentSize.width, IMAGE_HEIGHT)];
-    self.imageView.contentMode = UIViewContentModeScaleAspectFill;
-    self.imageView.clipsToBounds = YES;
+    //Set scroll view size based on image width.
+    self.imageWidthConstraint.constant = self.view.frame.size.width;
+    [self.scrollView setNeedsUpdateConstraints];
+    
+    //Set recipe image.
     [self.imageView setImageWithURL:[NSURL URLWithString:self.card.imageUrl]];
-    [self.scrollView addSubview:self.imageView];
     
     self.titleView.text = self.card.title;
     
     self.navBar.alpha = 0;
     
-    [self initInfoViews];
+//    [self initInfoViews];
+}
+
+- (void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+    
+    self.imageView.translatesAutoresizingMaskIntoConstraints = YES;
 }
 
 - (UIStatusBarStyle)preferredStatusBarStyle {
     return UIStatusBarStyleLightContent;
 }
 
-- (void)initInfoViews {
-    //Create labels' container.
-    UIView *infosContainer = [[UIView alloc] initWithFrame:CGRectMake(0, IMAGE_HEIGHT, self.scrollView.contentSize.width, INFOS_CONTAINER_HEIGHT)];
+- (void)setInfoLabels {
+    self.firstInfoTitle.text = [NSLocalizedString(@"healthy", nil) uppercaseString];
+    self.secondInfoTitle.text = [NSLocalizedString(@"time", nil) uppercaseString];
+    self.thirdInfoTitle.text = [NSLocalizedString(@"price", nil) uppercaseString];
     
-    float containerWidth = self.scrollView.contentSize.width / 3;
-    
-    //Create label views.
-    UILabel *firstInfoLabel = [[UILabel alloc] initWithFrame:CGRectMake(0,
-                                                                        INFO_LABEL_TOP_MARGIN,
-                                                                        containerWidth,
-                                                                        INFO_LABEL_HEIGHT)];
-    UILabel *secondInfoLabel = [[UILabel alloc] initWithFrame:CGRectMake(containerWidth,
-                                                                         INFO_LABEL_TOP_MARGIN,
-                                                                         containerWidth,
-                                                                         INFO_LABEL_HEIGHT)];
-    UILabel *thirdInfoLabel = [[UILabel alloc] initWithFrame:CGRectMake(2 * containerWidth,
-                                                                        INFO_LABEL_TOP_MARGIN,
-                                                                        containerWidth,
-                                                                        INFO_LABEL_HEIGHT)];
-    UILabel *firstInfoTitle = [[UILabel alloc] initWithFrame:CGRectMake(0,
-                                                                             INFO_LABEL_TOP_MARGIN + INFO_LABEL_HEIGHT,
-                                                                             containerWidth,
-                                                                             INFO_TITLE_LABEL_HEIGHT)];
-    UILabel *secondInfoTitle = [[UILabel alloc] initWithFrame:CGRectMake(containerWidth,
-                                                                              INFO_LABEL_TOP_MARGIN + INFO_LABEL_HEIGHT,
-                                                                              containerWidth,
-                                                                              INFO_TITLE_LABEL_HEIGHT)];
-    UILabel *thirdInfoTitle = [[UILabel alloc] initWithFrame:CGRectMake(2 * containerWidth,
-                                                                             INFO_LABEL_TOP_MARGIN + INFO_LABEL_HEIGHT,
-                                                                             containerWidth,
-                                                                             INFO_TITLE_LABEL_HEIGHT)];
-    
-    //Set labels' color.
-    firstInfoLabel.textColor = [RACGraphics red];
-    secondInfoLabel.textColor = [RACGraphics red];
-    thirdInfoLabel.textColor = [RACGraphics red];
-    firstInfoTitle.textColor = [UIColor lightGrayColor];
-    secondInfoTitle.textColor = [UIColor lightGrayColor];
-    thirdInfoTitle.textColor = [UIColor lightGrayColor];
-    
-    //Set labels' alignment.
-    firstInfoLabel.textAlignment = NSTextAlignmentCenter;
-    secondInfoLabel.textAlignment = NSTextAlignmentCenter;
-    thirdInfoLabel.textAlignment = NSTextAlignmentCenter;
-    firstInfoTitle.textAlignment = NSTextAlignmentCenter;
-    secondInfoTitle.textAlignment = NSTextAlignmentCenter;
-    thirdInfoTitle.textAlignment = NSTextAlignmentCenter;
-    
-    //Set labels' font.
-    firstInfoLabel.font = [UIFont fontWithName:@"HelveticaNeue-Light" size:INFO_LABEL_FONT_SIZE];
-    secondInfoLabel.font = [UIFont fontWithName:@"HelveticaNeue-Light" size:INFO_LABEL_FONT_SIZE];
-    thirdInfoLabel.font = [UIFont fontWithName:@"HelveticaNeue-Light" size:INFO_LABEL_FONT_SIZE];
-    firstInfoTitle.font = [UIFont fontWithName:@"HelveticaNeue-Light" size:INFO_TITLE_FONT_SIZE];
-    secondInfoTitle.font = [UIFont fontWithName:@"HelveticaNeue-Light" size:INFO_TITLE_FONT_SIZE];
-    thirdInfoTitle.font = [UIFont fontWithName:@"HelveticaNeue-Light" size:INFO_TITLE_FONT_SIZE];
-    
-    //Set labels' content.
-    firstInfoTitle.text = [NSLocalizedString(@"healthy", nil) uppercaseString];
-    secondInfoTitle.text = [NSLocalizedString(@"time", nil) uppercaseString];
-    thirdInfoTitle.text = [NSLocalizedString(@"price", nil) uppercaseString];
-    
-    firstInfoLabel.text = [NSString stringWithFormat:@"%ld%%", self.card.healthScore];
-    secondInfoLabel.text = [NSString stringWithFormat:@"%ld %@", self.card.duration, NSLocalizedString(@"min", nil)];
+    self.firstInfoView.text = [NSString stringWithFormat:@"%ld%%", self.card.healthScore];
+    self.secondInfoView.text = [NSString stringWithFormat:@"%ld %@", self.card.duration, NSLocalizedString(@"min", nil)];
     
     NSString *priceStr = @"";
     
@@ -129,28 +84,12 @@
         priceStr = [priceStr stringByAppendingString:@"$"];
     }
     
-    thirdInfoLabel.text = priceStr;
-    
-    //Add label subviews.
-    [infosContainer addSubview:firstInfoLabel];
-    [infosContainer addSubview:secondInfoLabel];
-    [infosContainer addSubview:thirdInfoLabel];
-    [infosContainer addSubview:firstInfoTitle];
-    [infosContainer addSubview:secondInfoTitle];
-    [infosContainer addSubview:thirdInfoTitle];
-    
-    UIView *separator = [[UIView alloc] initWithFrame:CGRectMake((infosContainer.frame.size.width - SEPARATOR_WIDTH)/2,
-                                                                 infosContainer.frame.size.height - SEPARATOR_HEIGHT,
-                                                                 SEPARATOR_WIDTH,
-                                                                 SEPARATOR_HEIGHT)];
-    separator.backgroundColor = [UIColor lightGrayColor];
-    [infosContainer addSubview:separator];
-    
-    [self.scrollView addSubview:infosContainer];
+    self.thirdInfoView.text = priceStr;
 }
 
+
 - (void)initIngredientsView {
-    NSArray *ingredients = @[@"1 pack of cherry tomatoes,
+    NSArray *ingredients = @[@"1 pack of cherry tomatoes",
                              @"1 pack of cherry size mozzarella",
                              @"1 pack of fresh basil leaves",
                              @"Olive oil",
@@ -162,9 +101,18 @@
         ingredientsString = [ingredientsString stringByAppendingFormat:@"%@\n", ingredient];
     }
     
-    UITextView *ingredientsView = [[UITextView alloc] initWithFrame:CGRectMake(0, 0, ingredientsString, <#CGFloat height#>)]
+//    UITextView *ingredientsView = [[UITextView alloc] initWithFrame:CGRectMake(0, 0, ingredientsString, <#CGFloat height#>)]
+//    
+//    UIView *ingredientsContainer = [[UIView alloc] initWithFrame:CGRectMake(0, IMAGE_HEIGHT + INFOS_CONTAINER_HEIGHT, <#CGFloat width#>, <#CGFloat height#>)
+}
+
+- (void) initRecipeStepView {
+    UITextView *descriptionView = [UITextView new];
+    descriptionView.text = @"This is a long description. This is a long description. This is a long description. This is a long description. This is a long description. This is a long description. \n This is a long description. This is a long description. \n This is a long description. This is a long description. This is a long description. This is a long description. This is a long description.";
+//    [descriptionView sizeThatFits:]
     
-    UIView *ingredientsContainer = [[UIView alloc] initWithFrame:CGRectMake(0, IMAGE_HEIGHT + INFOS_CONTAINER_HEIGHT, <#CGFloat width#>, <#CGFloat height#>)
+    
+//    UIView *stepView = [[UIView alloc] initWithFrame:<#(CGRect)#>]
 }
 
 - (IBAction)backButtonPressed:(id)sender {
